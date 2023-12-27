7a3c26f8237df9a33db846e72dc6b1e1ecdf5c58
ecolo9ger <ecolo9ger@gmail.com>
2023-12-27 23:51:35 +0900

    객체 생성하기 연습

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("콘솔로 문자가 나와요.");
+const member = {
+  name: "박영일",
+  phone: "01063190125",
+  member_id: "ecolo9ger",
+  email: "ecolo9ger@gmail.com",
+  address: [
+    {
+      receiverName: "박은실",
+      receiverPhone: "01024018788",
+      receiverAddress: "제주특별자치도 서귀포시 신서귀로9(법환동, 동남서호)",
+      address2: "108동 103호",
+    },
+    {
+        receiverName: "홍길동",
+        receiverPhone: "01063190125",
+        receiverAddress: "우주나라 어느 별",
+        address2: "깐따삐아",
+      },
+  ],
+};
+const member2 = member;
+
+member.address[0].receiverName = "박서아";
+
+console.log(member);
+console.log(member2);
+
+//깊은 속성값을 복사하려면 참조에 의한 변수이기 때문에 처음의 값도 같이 변하게 되므로 주의를 요함!!
+
+
